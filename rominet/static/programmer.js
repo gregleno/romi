cce50512eac6cd38463d2252b4444bafff46fffe
Gregory Lenoble <gregory.lenoble@gmail.com>
2017-10-06 23:17:57 +0200

    Improve kanban

--- rominet/static/programmer.js
+++ rominet/static/programmer.js
@@ -1,21 +1,27 @@
 function programmer_init(){
-     $("#buttonGroup").jqxButtonGroup({ mode: 'default' });
+    $("#Move_Forward").jqxButton({ width: 50, height: 50, imgWidth: 40, imgHeight: 40,
+                                    imgSrc: "static/images/arrow-orange-up-300px.png" });
+    $("#Move_Backward").jqxButton({ width: 50, height: 50, imgWidth: 40, imgHeight: 40,
+                                    imgSrc: "static/images/arrow-orange-down-300px.png" });
+    $("#Turn_Left").jqxButton({ width: 50, height: 50, imgWidth: 40, imgHeight: 40,
+                                imgSrc: "static/images/arrow-go-left-300px.png" });
+    $("#Turn_Right").jqxButton({ width: 50, height: 50, imgWidth: 40, imgHeight: 40,
+                                 imgSrc: "static/images/arrow-go-right-300px.png" });
 
     var fields = [
+              { name: "resourceId", type: "number" },
               { name: "status", map: "state", type: "string" },
-              { name: "text", map: "label", type: "string" },
               { name: "tags", type: "string" },
-              { name: "color", map: "hex", type: "string" },
-              { name: "resourceId", type: "number" }
+              { name: "text", map: "label", type: "string" }
+
     ];
     var source =
      {
          localData: [
-                  { state: "new", label: "Combine Orders", tags: "orders, combine", hex: "#5dc3f0", resourceId: 3 },
-                  { state: "new", label: "Change Billing Address", tags: "billing", hex: "#f19b60", resourceId: 1 },
-                  { state: "new", label: "One item added to the cart", tags: "cart", hex: "#5dc3f0", resourceId: 3 },
-                  { state: "new", label: "Edit Item Price", tags: "price, edit", hex: "#5dc3f0", resourceId: 4 },
-                  { state: "new", label: "Login 404 issue", tags: "issue, login", hex: "#6bbd49" }
+                  { state: "new", resourceId: 3, tags: " ", label: "r " },
+                  { state: "new", resourceId: 1, tags: " ", label: "r " },
+                  { state: "new", resourceId: 3, tags: " ", label: "r " },
+                  { state: "new", resourceId: 0, tags: " ", label: " " },
          ],
          dataType: "array",
          dataFields: fields
@@ -25,38 +31,39 @@ function programmer_init(){
         var resourcesSource =
         {
             localData: [
-                  { id: 0, name: "No name", image: "static/images/arrow-go-left-300px.png", common: true },
-                  { id: 1, name: "Andrew Fuller", image: "static/images/arrow-go-left-300px.png" },
-                  { id: 2, name: "Janet Leverling", image: "static/images/arrow-go-right-300px.png" },
-                  { id: 3, name: "Steven Buchanan", image: "static/images/arrow-go-left-300px.png" },
-                  { id: 4, name: "Nancy Davolio", image: "static/images/arrow-go-left-300px.png" },
-                  { id: 5, name: "Michael Buchanan", image: "../../images/Michael.png" },
-                  { id: 6, name: "Margaret Buchanan", image: "../../images/margaret.png" },
-                  { id: 7, name: "Robert Buchanan", image: "../../images/robert.png" },
-                  { id: 8, name: "Laura Buchanan", image: "../../images/Laura.png" },
-                  { id: 9, name: "Laura Buchanan", image: "../../images/Anne.png" }
+                  { id: 0, name: "Move Forward", image: "static/images/arrow-orange-up-300px.png"},
+                  { id: 1, name: "Move Backward", image: "static/images/arrow-orange-down-300px.png" },
+                  { id: 2, name: "Turn Left", image: "static/images/arrow-go-left-300px.png" },
+                  { id: 3, name: "Turn Right", image: "static/images/arrow-go-right-300px.png" },
             ],
             dataType: "array",
             dataFields: [
                  { name: "id", type: "number" },
                  { name: "name", type: "string" },
                  { name: "image", type: "string" },
-                 { name: "common", type: "boolean" }
             ]
         };
         var resourcesDataAdapter = new $.jqx.dataAdapter(resourcesSource);
         return resourcesDataAdapter;
     }
 
-
+     var templateContent = { status: "new", text: "", content: "", tags: "", color: "green", resourceId: 0, className: ""}
+     //$('#kanban1').jqxKanban({ templateContent: templateContent });
      $('#kanban1').jqxKanban({
-        width: '100%',
-        height: '100%',
+        template: "<div class='jqx-kanban-item' id=''>"
+                             + "<div class='jqx-kanban-item-color-status'></div>"
+                             + "<div style='display: none;' class='jqx-kanban-item-avatar'></div>"
+                             + "<div class='jqx-icon jqx-icon-close-white jqx-kanban-item-template-content jqx-kanban-template-icon'></div>"
+                             + "<div class='jqx-kanban-item-text'></div>"
+                             + "<div style='display: none;' class='jqx-kanban-item-footer'></div>"
+                     + "</div>",
+        width: '50%',
+        height: '600',
         resources: resourcesAdapterFunc(),
         source: dataAdapter,
         connectWith: "#kanban2, #kanban3",
         columns: [
-            { text: "Backlog", dataField: "new", maxItems: 10 }
+            { text: "Backlog", dataField: "new",  maxItems: 10 }
         ],
         columnRenderer: function (element, collapsedElement, column) {
             var columnItems = $("#kanban1").jqxKanban('getColumnItems', column.dataField).length;
